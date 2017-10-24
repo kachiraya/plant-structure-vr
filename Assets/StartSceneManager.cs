8dc2229a5be5c42f1117eb259f9dae1840238c27
Taeshawit Suwanwigo <s.taeshawit@icloud.com>
2017-10-25 02:29:32 +0700

    First scene integrated

---
+++ Assets/StartSceneManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSceneManager : MonoBehaviour {
+
+	public GameObject startCanvas;
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKey || Input.anyKeyDown) {
+			startCanvas.SetActive (false);
+			Time.timeScale = 1.0f;
+//			StartCoroutine (StartFade ());
+		}
+	}
+
+//	IEnumerator StartFade() {
+//		float fadeTime = GetComponent<Fader>().BeginFade (1);
+//		yield return new WaitForSeconds (fadeTime);
+//	}
+}
